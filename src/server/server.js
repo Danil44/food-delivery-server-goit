41ffe0fdbf8ea33ae9dfea9c01acd7069ddb9e04
danil <44bridgeprod@gmail.com>
2019-02-15 20:00:59 +0200

    problem with https

--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,16 @@
 const http = require("http");
 const url = require("url");
-
+const fs = require("fs");
 const morgan = require("morgan");
 const router = require("../routes/router");
+const getRouteHandler = require("../helpers/get-route-handler");
 
 const logger = morgan("combined");
 
 const startServer = port => {
   const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url);
-    const func = router[parsedUrl.pathname] || router.default;
+    const func = getRouteHandler(router, parsedUrl.pathname, parsedUrl.query) || router.default;
 
     logger(req, res, () => func(req, res));
   });
