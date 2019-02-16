519cfef7b442bb3bce3b0833c0ab9e63eee1ed5e
danil <44bridgeprod@gmail.com>
2019-02-16 18:30:42 +0200

    add homework-2

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,4 @@
-const http = require("http");
+const https = require("https");
 const url = require("url");
 const fs = require("fs");
 const morgan = require("morgan");
@@ -7,8 +7,14 @@ const getRouteHandler = require("../helpers/get-route-handler");
 
 const logger = morgan("combined");
 
+const options = {
+  key: fs.readFileSync(path.resolve("src/ssl/server.key")),
+  cert: fs.readFileSync(path.resolve("src/ssl/server.crt")),
+  csr: fs.readFileSync(path.resolve('src/ssl/server.csr'))
+};
+
 const startServer = port => {
-  const server = http.createServer((req, res) => {
+  const server = https.createServer(options, (req, res) => {
     const parsedUrl = url.parse(req.url);
     const func = getRouteHandler(router, parsedUrl.pathname, parsedUrl.query) || router.default;
 
